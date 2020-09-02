8d76bf4647e2b3695af2533db4d019f8ec18cd2e
Eduardo Carrasco Romero <blezthor666@gmail.com>
2020-09-01 21:31:50 -0400

    user service
    add user service

--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -1,52 +1,90 @@
-import { Injectable, forwardRef, Inject } from '@nestjs/common';
+import { Injectable, forwardRef, Inject, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectRepository } from "@nestjs/typeorm";
-import { User } from '../entity/user.entity';
+import { User } from '../../../entities/User.entity';
 import { Repository } from 'typeorm';
-import { CreateUserDv, UserData, UpdateUser } from '../interface/user.interface';
+import { UserDTO, UpdateUserDTO, CreateUserDTO } from '../dtos/user.DTO';
+import { toUserDto } from 'src/shared/mapper';
 
 @Injectable()
 export class UsersService {
     constructor(
-        @Inject('USER_REPOSITORY')
-            private userRepository: Repository<User>,
-            
-        ){}
+        @InjectRepository(User)
+            private readonly userRepository: Repository<User>,
+    ){}
 
-    
-    async createUser(user: CreateUserDv) {
-        let value = this.dgv(user.rut,user.rutDv)
+    async addUser(userDTO: CreateUserDTO): Promise<UserDTO> {
+        const { firstname, lastname, role, rutSD, rutDv,  phone, email, password } = userDTO;
+        let value = await this.dgv(rutSD,rutDv)
+        
         if (value===true) {
-            const USER = await this.save(user)
-            
-            return await this.userRepository.insert(USER)
-            .catch(err => {
-                console.log(err)
-            })
+            const rut = rutSD.toString().concat(rutDv.toString());
+            const userInDb = await this.userRepository.findOne({ 
+                where: { rut } 
+            }); 
+            if (userInDb) {
+                throw new HttpException('User already exists', HttpStatus.BAD_REQUEST);    
+            }
+            const user: User = await this.userRepository.create({
+                firstname,
+                lastname,
+                role,
+                rut,
+                phone,
+                email,
+                password,                
+            });
+
+            await this.userRepository.save(user)
+
+            return toUserDto(user);
         } else {
-            throw new ErrorEvent('user:create:missingRutValidation'); // REVISAR COMO HACER
+            throw new HttpException('Rut not valid', HttpStatus.BAD_REQUEST); // REVISAR COMO HACER
         } 
     }
-
-    async findAllUser(): Promise<User[]> {
+    
+    async findAllUser(): Promise<UserDTO[]> {
         return await this.userRepository.find();
     }
 
-    async findOneUser(options: any): Promise<User> {
+    async findOneUser(options: any): Promise<UserDTO> {
         return await this.userRepository.findOne(options);
-    }
+    } 
 
-    async updateUser(id: number, newValue: Partial<UpdateUser>){
-        return await this.userRepository.update({user_id: id}, newValue);
-    }
+    async findOneUserByEmail(email: string){
+        return await this.userRepository
+            .createQueryBuilder('user')
+            .where({ email })
+            .addSelect('user.password')
+            .getOne()
+    } 
 
-
-
+    // revisar
+    /* async findByLogin({ username, password }: LoginUserDto): Promise<UserDto> {
+        const user = await this.userRepo.findOne({ where: { username } });
+    
+        if (!user) {
+          throw new HttpException('User not found', HttpStatus.UNAUTHORIZED);
+        }
+    
+        // compare passwords
+        const areEqual = await comparePasswords(user.password, password);
+    
+        if (!areEqual) {
+          throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
+        }
+    
+        return toUserDto(user);
+    } */
+    /* async findByPayload({ username }: any): Promise<UserDTO> {
+        return await this.findOneUser({ where: { username } });
+    }*/
 
     async updateUser(id: number, newValue: UpdateUserDTO){
         return await this.userRepository.update({userId: id}, newValue);
     }
 
     private dgv(rut: number, dv: string){  
+        console.log(rut,dv)
         var M=0,S=1;
         for(;rut;rut=Math.floor(rut/10)){
         S=(S+rut%10*(9-M++%6))%11;}
