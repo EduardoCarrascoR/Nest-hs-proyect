d6adff010272051df57a9f02b809c594101ce3e6
Eduardo Carrasco Romero <blezthor666@gmail.com>
2020-09-02 16:13:57 -0400

    apply auth in controllers
    apply auth decorators in controllers routes

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -3,6 +3,7 @@ import { UsersService } from '../services/users.service';
 import { UpdateUserDTO, CreateUserDTO } from '../dtos/user.DTO';
 import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/guards';
+import { Auth } from 'src/common/decorators';
 
 @ApiTags('Users')
 @Controller('users')
@@ -10,19 +11,21 @@ export class UsersController {
     constructor(
         private readonly userService: UsersService
     ){}
-    @UseGuards( JwtAuthGuard )    
-    @ApiBearerAuth()
+
+    @Auth()
     @Get('/all')
     async getUsers(@Res() res) {
         const users = await this.userService.findAllUser();
         return res.status(HttpStatus.OK).json(users)
     }
 
+    @Auth()
     @Post()
     async createUser(@Body() userdto: CreateUserDTO ) {
         const user = await this.userService.addUser(userdto)
     }
 
+    @Auth()
     @Put(':id')
     async updateUser(@Body() changes: UpdateUserDTO, @Param() params) {
         if(!params.id) throw new BadRequestException("Can't update user id")
