2b76976dbdb3d1ca37bfbb3f88c747693dfc694e
Eduardo C <blezthor666@gmail.com>
2020-12-28 13:59:39 -0300

    changes in modules
    changes in client module and news services

--- src/modules/clients/controllers/clients.controller.ts
+++ src/modules/clients/controllers/clients.controller.ts
@@ -1,8 +1,10 @@
-import { BadRequestException, Body, Controller, Get, HttpException, HttpStatus, Param, Post, Put, Res } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, HttpException, HttpStatus, Param, ParseIntPipe, Post, Put, Query, Res } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { validate } from 'class-validator';
+import { DatePipePipe } from 'src/pipes/date-pipe.pipe';
 import { Auth } from '../../../common/decorators';
 import { AppResources } from '../../../common/enums';
-import { ClientDTO, EditClientDto } from '../dtos/';
+import { ClientDTO, EditClientDto, ReportClientDTO } from '../dtos/';
 import { ClientsService } from '../services/clients.service';
 
 @ApiTags('Clients')
@@ -22,6 +24,21 @@ export class ClientsController {
         const clients = await this.clientService.findAllClients();
         if(clients.length == 0) throw new HttpException({ success: false, status: HttpStatus.NOT_FOUND, message: "Clients not found" }, HttpStatus.NOT_FOUND)
         return res.status(HttpStatus.ACCEPTED).json({ success: true, clients: clients })
+    }
+
+    @Auth({
+        possession: 'any',
+        action: 'read',
+        resource: AppResources.CLIENT,
+    })
+    @Get('/reportClient/:clientId/date/:date')
+    async getReportsClient(@Param('clientId', ParseIntPipe) clientId: number, @Param('date', new DatePipePipe()) date, @Res() res) {
+        if(!clientId) throw new HttpException({ success: false, status: HttpStatus.BAD_REQUEST, message: "Client Id not joined." }, HttpStatus.BAD_REQUEST)
+        if(!date) throw new HttpException({ success: false, status: HttpStatus.BAD_REQUEST, message: "Date not joined." }, HttpStatus.BAD_REQUEST)
+
+        const reports = await this.clientService.findAllreportClient(clientId,date);
+        if(reports.length == 0) throw new HttpException({ success: true, status: HttpStatus.ACCEPTED, message: "Reports not found", reports }, HttpStatus.ACCEPTED)
+        return res.status(HttpStatus.ACCEPTED).json({ success: true, reports: reports })
     }
 
     @Auth({
