736508ef9a501f93dd77a7527541a3704d3658fd
Eduardo C <blezthor666@gmail.com>
2021-01-22 17:32:12 -0300

    debug default user function

--- src/config/default-user.ts
+++ src/config/default-user.ts
@@ -1,25 +1,32 @@
 import { ConfigService } from "@nestjs/config";
-import { getRepository } from "typeorm";
+import { getConnection, getRepository } from "typeorm";
 import { User } from '../entities';
-import { DEFAULT_USER_EMAIL, DEFAULT_USER_PASSWORD, DEFAULT_USER_RUT, DEFAULT_USER_FIRSTNAME } from "./constants";
+import { DEFAULT_USER_EMAIL, DEFAULT_USER_PASSWORD, DEFAULT_USER_RUT, DEFAULT_USER_FIRSTNAME, DEFAULT_USER_LASTNAME, DEFAULT_USER_PHONE } from "./constants";
 
 
 export const setDefaultUser = async (config: ConfigService) => {
-    const userRepository = getRepository<User>(User)
-
-    const defaultUser = await userRepository
-        .createQueryBuilder()
-        .where('rut = rut', { rut: config.get<string>(DEFAULT_USER_RUT)})
-        .getOne()
-    if(!defaultUser) {
-        const adminUser = userRepository.create({
-            email: config.get<string>(DEFAULT_USER_EMAIL),
-            firstname: config.get<string>(DEFAULT_USER_FIRSTNAME),
-            password: config.get<string>(DEFAULT_USER_PASSWORD),
-            rut: config.get<string>(DEFAULT_USER_RUT),
-            roles: ['Admin']
-        })
-
-        return await userRepository.save(adminUser)
+    let defaultUser;
+    const user = {
+        email: config.get<string>(DEFAULT_USER_EMAIL),
+        firstname: config.get<string>(DEFAULT_USER_FIRSTNAME),
+        lastname: config.get<string>(DEFAULT_USER_LASTNAME),
+        password: config.get<string>(DEFAULT_USER_PASSWORD),
+        rut: config.get<string>(DEFAULT_USER_RUT),
+        phone: config.get<string>(DEFAULT_USER_PHONE),
+        roles: ['Admin']
     }
+    
+    await getConnection().transaction(async transaction => {
+        defaultUser = await transaction
+            .createQueryBuilder()
+            .select("user")
+            .from(User, "user")
+            .where("user.rut = :userRut", { userRut: config.get<string>(DEFAULT_USER_RUT)})
+            .getOne();
+        if(!defaultUser) {
+            const adminUser = transaction.create(User,user)
+    
+            return await transaction.save(adminUser)
+        }
+    })
 }
